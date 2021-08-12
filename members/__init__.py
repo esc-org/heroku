8fcda8586f817f2eebb6c9ad9f2f5afcbc76d9e4
Laddge <dev.laddge@gmail.com>
2021-08-12 13:50:15 +0900

    [fix] solve errors

--- members/__init__.py
+++ members/__init__.py
@@ -7,23 +7,24 @@ API = tweepy.API(auth)
 
 
 def main():
-    if float(os.getenv('MEMBER_FETCHED')) + 900 >= time.time():
-        resdict = os.getenv('MEMBER_RESDICT')
-    else:
-        resdict = {'users': []}
-        for user in tweepy.Cursor(
-            API.list_members, slug=os.getenv('TWLIST_SLUG'), owner_screen_name='laddge_'
-        ).items():
-            d = {
-                'name': user.name,
-                'screen_name': user.screen_name,
-                'image': user.profile_image_url.replace('_normal', ''),
-                'description': user.description,
-            }
-            resdict['users'].append(d)
-        os.environ['MEMBER_RESDICT'] = str(resdict)
-        os.environ['MEMBER_FETCHED'] = str(time.time())
-        print('updated members dict')
+    if os.getenv('MEMBER_FETCHED'):
+        if float(os.getenv('MEMBER_FETCHED')) + 900 >= time.time():
+            resdict = os.getenv('MEMBER_RESDICT')
+            return resdict
+    resdict = {'users': []}
+    for user in tweepy.Cursor(
+        API.list_members, slug=os.getenv('TWLIST_SLUG'), owner_screen_name='laddge_'
+    ).items():
+        d = {
+            'name': user.name,
+            'screen_name': user.screen_name,
+            'image': user.profile_image_url.replace('_normal', ''),
+            'description': user.description,
+        }
+        resdict['users'].append(d)
+    os.environ['MEMBER_RESDICT'] = str(resdict)
+    os.environ['MEMBER_FETCHED'] = str(time.time())
+    print('updated members dict')
     return resdict
 
 
